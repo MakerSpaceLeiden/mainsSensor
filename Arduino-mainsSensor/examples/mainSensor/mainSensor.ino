578719e01d3ee88c7b8b16d153aac02a301dcac2
Dirk-Willem van Gulik <dirkx@webweaving.org>
2019-09-08 21:28:26 +0200

    Show actual time stamp

--- Arduino-mainsSensor/examples/mainSensor/mainSensor.ino
+++ Arduino-mainsSensor/examples/mainSensor/mainSensor.ino
@@ -28,17 +28,19 @@ void setup() {
   pinMode(GPIO_INPUT_PIN, INPUT);
   msr.setup();
   msr.begin();
+#if 0
   msr.setRawcb([](rmt_data_t * items, size_t len) {
     Serial.printf("CB: %d items\n", len);
     for (int i = 0; i < len; i++) {
       if (items[i].duration0)
         Serial.printf("%d: L=%d %8d # %12.2f nSecs\n", i * 2,
-                      items[i].level0, items[i].duration0, 0. /* msr.nanoseconds(items[i].duration0) */);
+                      items[i].level0, items[i].duration0, msr.nanoseconds(items[i].duration0));
       if (items[i].duration1)
         Serial.printf("%d: L=%d %8d # %12.2f nSecs\n", i * 2 + 1,
-                      items[i].level1, items[i].duration1, 0. /* msr.nanoseconds(items[i].duration1) */);
+                      items[i].level1, items[i].duration1, msr.nanoseconds(items[i].duration1));
     };
   });
+#endif
 };
 
 void loop() {
